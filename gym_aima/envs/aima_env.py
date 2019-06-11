eda0adc7384746436d6ff4ac69511fd986a066eb
Miguel Morales <mimoralea@gmail.com>
2019-06-10 23:08:43 -0700

    Create initial gridworld

--- gym_aima/envs/aima_env.py
+++ gym_aima/envs/aima_env.py
@@ -1,49 +1,87 @@
 import sys
+from contextlib import closing
+
 import numpy as np
 from six import StringIO, b
-from string import ascii_uppercase
 
 from gym import utils
 from gym.envs.toy_text import discrete
 
-LEFT, RIGHT = 0, 1
+LEFT, DOWN, RIGHT, UP = range(4)
+
+MAPS = {
+    "3x4": [
+        "FFFG",
+        "FWFH",
+        "SFFF",
+    ],
+}
 
 class AIMAEnv(discrete.DiscreteEnv):
 
     metadata = {'render.modes': ['human', 'ansi']}
 
-    def __init__(self, n_states=5):
+    def __init__(self, map_name="3x4", noise=0.2, living_rew=0.0):
+        desc = MAPS[map_name]
+        self.desc = desc = np.asarray(desc, dtype='c')
+        self.nrow, self.ncol = nrow, ncol = desc.shape
 
-        self.shape = (1, n_states)
-        self.start_state_index = self.shape[1]//2
+        nA = 4
+        nS = nrow * ncol
 
-        self.nS = nS = np.prod(self.shape)
-        self.nA = nA = 2
+        isd = np.array(desc == b'S').astype('float64').ravel()
+        isd /= isd.sum()
 
-        P = {}
-        for s in range(nS):
-            P[s] = {}
-            for a in range(nA):
-                prob = 1.0
-                new_state = np.clip(s - 1 if a == LEFT else s + 1, 0, nS - 1)
-                new_state = s if s == 0 or s == nS - 1 else new_state
-                reward = 1.0 if new_state == nS - 1 and s != new_state else 0.0
-                is_terminal = new_state == 0 or new_state == nS - 1
-                P[s][a] = [(prob, new_state, reward, is_terminal)]
+        P = {s : {a : [] for a in range(nA)} for s in range(nS)}
 
-        isd = np.zeros(nS)
-        isd[self.start_state_index] = 1.0
+        def to_s(row, col):
+            return row*ncol + col
 
-        discrete.DiscreteEnv.__init__(self, nS, nA, P, isd)
+        def inc(row, col, a):
+            if a == LEFT:
+                col = max(col-1, 0)
+            elif a == DOWN:
+                row = min(row+1, nrow-1)
+            elif a == RIGHT:
+                col = min(col+1, ncol-1)
+            elif a == UP:
+                row = max(row-1, 0)
+            return (row, col)
 
-    def render(self, mode='human', close=False):
+        for row in range(nrow):
+            for col in range(ncol):
+                s = to_s(row, col)
+                for a in range(4):
+                    li = P[s][a]
+                    letter = desc[row, col]
+                    if letter in b'GHW':
+                        li.append((1.0, s, 0, True))
+                    else:
+                        for b in [(a-1) % 4, a, (a+1) % 4]:
+                            newrow, newcol = inc(row, col, b)
+                            newstate = to_s(newrow, newcol)
+                            newletter = desc[newrow, newcol]
+                            newstate = s if newletter in b'W' else newstate
+                            done = bytes(newletter) in b'GH'
+                            rew = 1.0 if newletter == b'G' else -1.0 if newletter == b'H' else living_rew
+                            li.append((np.round(1.0-noise if a == b else noise/2., 2), newstate, rew, done))
+
+        super(AIMAEnv, self).__init__(nS, nA, P, isd)
+
+    def render(self, mode='human'):
         outfile = StringIO() if mode == 'ansi' else sys.stdout
-        desc = np.asarray([ascii_uppercase[:self.shape[1]]], dtype='c').tolist()
+
+        row, col = self.s // self.ncol, self.s % self.ncol
+        desc = self.desc.tolist()
         desc = [[c.decode('utf-8') for c in line] for line in desc]
-        color = 'red' if self.s == 0 else 'green' if self.s == self.nS - 1 else 'yellow'
-        desc[0][self.s] = utils.colorize(desc[0][self.s], color, highlight=True)
-        outfile.write("\n")
+        desc[row][col] = utils.colorize(desc[row][col], "red", highlight=True)
+        if self.lastaction is not None:
+            outfile.write("  ({})\n".format(["Left", "Down", "Right", "Up"][self.lastaction]))
+        else:
+            outfile.write("\n")
         outfile.write("\n".join(''.join(line) for line in desc)+"\n")
 
         if mode != 'human':
-            return outfile
+            with closing(outfile):
+                return outfile.getvalue()
+
