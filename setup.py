73ef5fe6561e9ef4b629f17f0471aa9aba834867
Martin Bayly <Martin.Bayly@D2L.com>
2022-03-27 12:03:42 -0700

    update to use gym 0.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gym_aima',
-    version='0.0.1',
+    version='0.0.2',
     description='Gym Gridworld environment of the Artificial Intelligence a Modern Approach',
     url='https://github.com/mimoralea/gym-aima',
     author='Miguel Morales',
